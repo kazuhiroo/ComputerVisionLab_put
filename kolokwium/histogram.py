995e7e8894f230a37ca437d7e48c72d10308be3c
kazuhiroo <katowski.antoni@gmail.com>
2025-11-23 16:07:02 +0100

    added varies of tasks

---
+++ kolokwium/histogram.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+img_g = cv2.imread("strawberry.jpg", cv2.IMREAD_GRAYSCALE)
+img_c = cv2.imread("strawberry.jpg")
+
+img_g = cv2.resize(img_g, dsize=(0,0), fx=0.5, fy=0.5)
+img_g = cv2.resize(img_c, dsize=(0,0), fx=0.5, fy=0.5)
+
+hist = cv2.calcHist([img_g],[0],None,[256],[0,256])
+plt.plot(hist)
+plt.xlim([0,256])
+plt.show()
+
+colours = ['b','g','r']
+
+for i, col in enumerate(colours):
+    hist = cv2.calcHist([img_c],[i],None,[256],[0,256])
+    plt.plot(hist, color = colours[i])
+    plt.xlim([0,256])
+plt.show()
+
+
+cv2.imshow('window', img_g)
+cv2.waitKey(0)
